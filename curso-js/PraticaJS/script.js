b1bd1ba581b2fd2f925cbcf5c0077e1b57ae6660
Emanuel Santiago Maia <maia06programa@gmail.com>
2023-05-12 16:21:50 -0300

    fiz correções

---
+++ curso-js/PraticaJS/script.js
@@ -0,0 +1,16 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    
+    msg.innerHTML = `São ${hora} horas!`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+    }else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+    }else {
+        img.src = 'imagens/noite.png'
+    }
+  
+}
